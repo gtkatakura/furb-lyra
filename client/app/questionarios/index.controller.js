401b3d5d4c6d2b1526229363e87a30136a6c5d9f
gtkatakura <gt.katakura@gmail.com>
2016-05-11 00:09:37 -0300

    [ADD] Adicionado funcionalidade de exclusão do Questionário na listagem

--- client/app/questionarios/index.controller.js
+++ client/app/questionarios/index.controller.js
@@ -1,7 +1,19 @@
 angular.module('lyra').controller('questionarioIndexCtrl', function($scope, questionarioApi) {
     $scope.questionarios = [];
-    
-    questionarioApi.all().success(function(questionarios) {
-        $scope.questionarios = questionarios;
-    });
+
+    $scope.excluir = function(questionario) {
+        if (confirm('Você tem certeza que deseja excluir esse questionário?')) {
+            questionarioApi.remove(questionario).success(function() {
+                carregarQuestionarios();
+            });
+        }
+    };
+
+    var carregarQuestionarios = function() {
+        questionarioApi.all().success(function(questionarios) {
+            $scope.questionarios = questionarios;
+        });
+    };
+
+    carregarQuestionarios();
 });
