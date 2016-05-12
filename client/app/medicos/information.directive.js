8cd99072921672ac17c18a5cc2b5033404cffd5f
alessonBernardo <alesson_Rbernardo@hotmail.com>
2016-05-11 22:44:58 -0300

    [RECTORING] Ajustes no serviço de mantimento de médicos.

--- client/app/medicos/information.directive.js
+++ client/app/medicos/information.directive.js
@@ -1,6 +1,6 @@
 angular.module('lyra').directive('medicoInformation', function() {
     return {
         restrict: 'AE',
-        templateUrl: 'app/medico/information.html'
+        templateUrl: 'app/medicos/information.html'
     };
 });
