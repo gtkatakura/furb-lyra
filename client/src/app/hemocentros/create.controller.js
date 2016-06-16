b7b6261d85b5bdbf2422a6a5949e5e8014dbd2c7
gtkatakura <gt.katakura@gmail.com>
2016-06-16 00:51:02 -0300

    fix(hemocentro): não estava salvando o hemocentro por falta do ativo

--- client/src/app/hemocentros/create.controller.js
+++ client/src/app/hemocentros/create.controller.js
@@ -1,6 +1,7 @@
 angular.module('lyra').controller('hemocentroCreateCtrl', function($scope, hemocentroApi) {
 
     $scope.save = function(hemocentro) {
+        hemocentro.ativo = true;
         hemocentroApi.save(hemocentro).success(function() {
             alert("Hemocentro criado com sucesso!");
         });
